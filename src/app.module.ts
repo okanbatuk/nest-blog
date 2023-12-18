f392f01eae3b6f3fdfd24aab13ff2edab06f41cb
myrn <okanbatuk@protonmail.com>
2023-12-18 15:17:32 +0300

    Added config module and typeorm config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,28 @@
 import { Module } from '@nestjs/common';
+import { UsersModule } from './models/users/users.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import entities from './database/factories';
 
 @Module({
-  imports: [],
-  controllers: [],
-  providers: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        database: configService.get('DB_NAME'),
+        entities,
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { ConfigService } from '@nestjs/config';
 
 (async () => {
   const app = await NestFactory.create(AppModule, {
     logger: ['error', 'warn'],
   });
+  const configService = app.get(ConfigService);
+  const PORT = configService.get<number>('PORT');
   app.use(helmet());
   app.setGlobalPrefix('api');
-  await app.listen(3000);
+  await app.listen(PORT);
 })();
